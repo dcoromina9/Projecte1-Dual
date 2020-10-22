14912bbe2c03b0087f07324894d909de48293faa
unknown <dcoromina8@boscdelacoma.cat>
2020-10-22 15:37:24 +0200

    pagina joc

--- joc/script.js
+++ joc/script.js
@@ -2,6 +2,19 @@ var winningMessage = ("You Won!");
 var lostMessage = ("You Lost!");
 var drawMessage = ("Its a Draw!");
 
+const winCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  
+  [0, 4, 8],
+  [2, 4, 6]
+];
+
 var cell1;
 var cell2;
 var cell3;
